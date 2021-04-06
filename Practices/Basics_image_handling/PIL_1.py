6db992c2f3afae0f27a09dc11d2de4efb6f90d67
oyewunmio <oluwaseyioyewunmi99@gmail.com>
2021-04-06 23:32:22 +0000

    first commit

---
+++ Practices/Basics_image_handling/PIL_1.py
@@ -0,0 +1,8 @@
+# python imaging library is used to provide general image handling and basic operations
+
+from PIL import Image # importing modules from pil
+
+#reading images 
+image = Image.open('/home/anonymous/Documents/Workplace/dataScience_work/seeds_classification/fwdtheseeds/BA-1.jpg')
+
+#reading images and converting it to grayscale
